0c7d15fb07b294cebc87d310ecfc5e185d88109b
duncky <d@duncky.com>
2013-03-20 19:24:24 +0000

    Functions outside class definition

--- main.js
+++ main.js
@@ -21,92 +21,107 @@ var slider = (function(slider){
       },
       length: {
         value: numItems
-      }
-    });
-    Object.defineProperties(Grid.prototype,{
-      pos: {
-        value: function(n,m) {
-          return this.grid[n][m];
-        }
       },
-      isSolved: {
-        value: function() {
-          return this.correct === this.grid.join();
-        }
-      },
-      shuffle: {
-        value: function() {
-          for (var i=this.length-1; i>0; i--) {
-            var r = Math.floor(Math.random() * (i+1));
-            var tmp = this.sequence[i];
-            this.sequence[i] = this.sequence[r];
-            this.sequence[r] = tmp;
-          }
-          return this.sequence;
-        }
-      },
-      structure: {
-        value: function() {
-            this.grid = [];
-            var temp = [];
-            for (var i=0; i <= this.length; i++) {
-              if (i%size === 0 && i !== 0) {
-                this.grid.push(temp);
-                temp = [ this.sequence[i] ];
-              } else {
-                temp.push( this.sequence[i] );
-              }
-            }
-            return this.grid;
-        }
-      },
-      itemClicked: {
-        value: function(n,m) {
-          this.canMove(n,m);
-        }
-      },
-      canMove: {
-        value: function(n,m) {
-          var max = size - 1;
-          var value = this.pos(n,m);
-          if (value !== '') {
-            if (n>0 && this.pos(n-1,m) === '') {
-              this.swap(n,m,n-1,m);
-            }
-            else if (n<max && this.pos(n+1,m) === '') {
-              this.swap(n,m,n+1,m);
-            }
-            else if (m>0 && this.pos(n,m-1) === '') {
-              this.swap(n,m,n,m-1);
-            }
-            else if (m<max && this.pos(n,m+1) === '') {
-              this.swap(n,m,n,m+1);
-            } else {
-              return false;
-            }
-          }
-        }
-      },
-      swap: {
-        value: function(n,m,p,q) {
-          var value = grid[n][m];
-          grid[p][q] = value;
-          grid[n][m] = '';
-
-          //TOFIX: moving dom element logic shouldn't be here
-          //TODO: add moving animation
-          var first = $('#' + n + m);
-          first.html('');
-          first.addClass('empty');
-
-          var second = $('#' + p + q);
-          second.html(value);
-          second.removeClass('empty');
-          return this;
-        }
+      columns: {
+        value: size
       }
     });
   };
+
+  Object.defineProperties(Grid.prototype,{
+    pos: {
+      value: function() {
+        // console.log(arguments[0] + ' ' + arguments[1]
+          // + ' ' + arguments[2]);
+        if (arguments.length === 3) {
+          this.grid[arguments[0]][arguments[1]] = arguments[2];
+        }
+        return this.grid[arguments[0]][arguments[1]];
+      }
+    },
+    isSolved: {
+      value: function() {
+        return this.correct === this.grid.join();
+      }
+    },
+    shuffle: {
+      value: function() {
+        for (var i=this.length-1; i>0; i--) {
+          var r = Math.floor(Math.random() * (i+1));
+          var tmp = this.sequence[i];
+          this.sequence[i] = this.sequence[r];
+          this.sequence[r] = tmp;
+        }
+        return this.sequence;
+      }
+    },
+    structure: {
+      value: function() {
+          this.grid = [];
+          var temp = [];
+          for (var i=0; i <= this.length; i++) {
+            if (i%this.columns === 0 && i !== 0) {
+              this.grid.push(temp);
+              temp = [ this.sequence[i] ];
+            } else {
+              temp.push( this.sequence[i] );
+            }
+          }
+          return this.grid;
+      }
+    },
+    itemClicked: {
+      value: function(n,m) {
+        this.canMove(n,m);
+      }
+    },
+    canMove: {
+      value: function(n,m) {
+        var max = this.columns - 1;
+        var value = this.pos(n,m);
+        if (value !== '') {
+          if (n>0 && this.pos(n-1,m) === '') {
+            this.swap(n,m,n-1,m);
+          }
+          else if (n<max && this.pos(n+1,m) === '') {
+            this.swap(n,m,n+1,m);
+          }
+          else if (m>0 && this.pos(n,m-1) === '') {
+            this.swap(n,m,n,m-1);
+          }
+          else if (m<max && this.pos(n,m+1) === '') {
+            this.swap(n,m,n,m+1);
+          } else {
+            return false;
+          }
+        }
+      }
+    },
+    swap: {
+      value: function(n,m,p,q) {
+        // var value = this.pos(n,m);
+        // this.pos(n,m,value);
+        // this.pos(n,m,'');
+        var value = this.grid[n][m];
+        this.grid[p][q] = value;
+        this.grid[n][m] = '';
+
+        //TOFIX: moving dom element logic shouldn't be here
+        //TODO: add moving animation
+        $('#' + n + m).addClass('empty')
+                  .html('');
+        $('#' + p + q).removeClass('empty')
+                  .html(value);
+        return this;
+      }
+    },
+    reset: {
+      value: function(size) {
+        this.grid = new Grid(size);
+        return 0;
+      }
+    }
+  });
 
   var Game = function(size) {
     var grid = new Grid(size);
@@ -117,53 +132,52 @@ var slider = (function(slider){
       grid: {
         value: grid
       },
+      columns: {
+        value: size
+      },
       elements: {
         value: $('#grid')
       }
     });
-    Object.defineProperties(Game.prototype,{
-      draw: {
-        value: function() {
+  };
+  Object.defineProperties(Game.prototype,{
+    draw: {
+      value: function() {
 
-          for (var n=0; n<size; n++) {
-            var column = $('<tr/>', {
-                class: 'columns'
-            }).appendTo(this.elements);
+        for (var n=0; n<this.columns; n++) {
+          var column = $('<tr/>', {
+              class: 'columns'
+          }).appendTo(this.elements);
 
-            //TODO: jQuery chaining
-            for (var m=0; m<size; m++) {
-              var value = this.grid.pos(n,m);
+          for (var m=0; m<this.columns; m++) {
+            var value = this.grid.pos(n,m);
 
-              var item = $('<td/>', {
-                  id: '' + n + m,
-                  class: 'item',
-                  html: value,
-                  click: function(g,n,m) {
-                    return function() {
-                      g.itemClicked(n,m);
-                    };
-                  }(this.grid,n,m)
-              }).appendTo(column);
+            var item = $('<td/>', {
+                id: '' + n + m,
+                class: 'item',
+                html: value,
+                click: function(g,n,m) {
+                  return function() {
+                    g.itemClicked(n,m);
+                  };
+                }(this.grid,n,m)
+            }).appendTo(column);
 
-              if (value === '') {
-                item.addClass('empty');
-              }
+            if (value === '') {
+              item.addClass('empty');
             }
           }
-          return this;
         }
-      },
-      reset: {
-        value: function(size) {
-          this.grid = new Grid(size);
-          grid.shuffle();
-          grid.structure();
-          this.draw();
-          return this;
-        }
+        return this;
       }
-    });
-  };
+    },
+    reset: {
+      value: function(size) {
+        this.grid.reset(size);
+      }
+    }
+  });
+
 
   slider.createGame = function(size) {
     var game = new Game(size);
@@ -181,5 +195,5 @@ var slider = (function(slider){
 var game = slider.createGame(5);
 $('.drop').change(function() {
   // slider.createGame($(this).val());
-  game.resetGame($(this).val());
+  game.reset($(this).val());
 });
